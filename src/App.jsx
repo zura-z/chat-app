24d6d5532081a8f34f91bc9c3527929bd4c4d5f2
Zurab Zaridze <zaridzezuri@gmail.com>
2023-04-14 20:49:55 +0400

    Add additional if statement for "user" object to prevent chat room leak
    ...before user signs in

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import LogOut from "./components/LogOut/LogOut";
 
 import { UserAuth } from './context/AuthContext'
 
-function App() {
+function App() {  
   const {user} = UserAuth();
 
   return (
     <div>
       
-      {user ? <LogOut /> : <SignIn />}
+      {user && user.displayName || user && user.isAnonymous ? <LogOut /> : <SignIn />}
 
     </div>
   );
